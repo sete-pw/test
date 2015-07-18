71cd621faaca99b8614af8abf82b63e05777a01d
Sergei Terehov <u1@dev.sete.pw>
2015-07-18 21:51:38 +0400

    test index with get args

--- private/www/index.php
+++ private/www/index.php
@@ -1,8 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1>
 <?
 	CO::RE()->name = 'Test project';
 
 	CO::RE()->hello = function(){
 		echo 'Hello, this is ' . CO::RE()->name . '!';
 	};
-	
+
 	CO::RE()->hello();
+?>
+</h1>
+
+
+
+<?
+	if(isset(CO::RE()->get['login'])){
+?>
+Пользователь авторизован
+<?
+	}else{
+?>
+Пользователь <strong>не авторизован</strong>
+<?
+	}
+?>
+</body>
+</html>
