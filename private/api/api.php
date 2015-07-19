0d5967fbabb2922aa59c78505aad2d8fb444d72a
smile <smile.nikitoz@gmail.com>
2015-07-20 01:11:26 +0400

    Api v0.2.6

--- private/api/api.php
+++ private/api/api.php
@@ -1,4 +1,4 @@
 <?
 	require_once(DIR_ROOT . 'api/apiCore.php');
-	$ApiCore = new ApiCore(CO::RE()->api,CO::RE()->get);
+	$ApiCore = new ApiCore(CO::RE()->api,CO::RE()->get['params']);
 	print_r($ApiCore->callMethod());
