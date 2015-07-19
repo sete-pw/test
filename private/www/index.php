12ad754335a05353cf05c5603dc8a17003f808e5
NuclearFukuRo <toffa@mail.ru>
2015-07-19 12:35:29 +0300

    приделал некоторые вещи
    Табличку можно сделать прям в контенте страницы.
    Также отображается вся информация, что и корзине, только еще и имя
    пользователя.
    В столбце Операции должна быть еще кнопка Редактировать, которая
    вызывает модальное окно.

--- private/www/index.php
+++ private/www/index.php
@@ -16,15 +16,10 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<style>
-		.content { 
-			padding-top: 45px;
-		}
-		</style>
-		<link href="/custom-style.css" rel="stylesheet">
+		<link href="../../assets/css/custom-style.css" rel="stylesheet">
 	</head>
 	<body>
-		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<nav class="container navbar navbar-default navbar-fixed-top" role="navigation">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -204,9 +199,22 @@
 			</div>
 		</div>
 
-		<div class="content" style="height:1000px;">
-			<h1>ПАНЕЛЬ УПРАВЛЕНИЯ</h1>
-<div class="abyrvalg">1</div>
+		<div class="modal fade" id="set_rowModal" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove"></span></button>
+						<h4 class="modal-title">Редактировать</h4>
+					</div>
+					<div class="modal-body">		
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container" id="content">
+    		<h1>ПАНЕЛЬ УПРАВЛЕНИЯ</h1>
 			<!-- ПАНЕЛЬ УПРАВЛЕНИЯ -->
 			<table class="table table-striped table-hover ">
 				<thead>
@@ -226,7 +234,7 @@
 						<td>2</td>
 						<td>3 ряд, 4 место</td>
 						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
+						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></a> &nbsp; <a href="#set_rowModal"  data-toggle="modal" class="text-primary" title="Редактировать"><span class="glyphicon glyphicon-file"></span></a> </td>
 					</tr>
 					<tr>
 						<td>1488</td>
@@ -234,7 +242,7 @@
 						<td>2</td>
 						<td>3 ряд, 4 место</td>
 						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
+						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></a> &nbsp; <a href="#set_rowModal"  data-toggle="modal" class="text-primary" title="Редактировать"><span class="glyphicon glyphicon-file"></span></a></td>
 					</tr>
 					<tr>
 						<td>1488</td>
@@ -242,39 +250,7 @@
 						<td>2</td>
 						<td>3 ряд, 4 место</td>
 						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
-					</tr>
-					<tr>
-						<td>1488</td>
-						<td>13</td>
-						<td>2</td>
-						<td>3 ряд, 4 место</td>
-						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
-					</tr>
-					<tr>
-						<td>1488</td>
-						<td>13</td>
-						<td>2</td>
-						<td>3 ряд, 4 место</td>
-						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
-					</tr>
-					<tr>
-						<td>1488</td>
-						<td>13</td>
-						<td>2</td>
-						<td>3 ряд, 4 место</td>
-						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
-					</tr>
-					<tr>
-						<td>1488</td>
-						<td>13</td>
-						<td>2</td>
-						<td>3 ряд, 4 место</td>
-						<td>2500 руб.</td>
-						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></td>
+						<td><a href="#del_row" class="text-danger" title="Удалить"><span class="glyphicon glyphicon-remove"></span></a> &nbsp; <a href="#set_rowModal"  data-toggle="modal" class="text-primary" title="Редактировать"><span class="glyphicon glyphicon-file"></span></a></td>
 					</tr>
 				</tbody>
 				<tbody>
@@ -283,14 +259,14 @@
 						<td> </td>
 						<td> </td>
 						<td> </td>
-						<td>17500 руб.</td>
+						<td>7500 руб.</td>
 						<td><a href="#del_row" class="text-danger" title="Удалить Всё"><span class="glyphicon glyphicon-remove"></span></td>
 					</tr>
 				</tbody>
 			</table>
 			<!-- END ПАНЕЛЬ УПРАВЛЕНИЯ -->
 		</div>
-
+		
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
