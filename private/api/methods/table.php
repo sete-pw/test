e01580f1e5ed004a37e408c2e554d5640099f122
smile <smile.nikitoz@gmail.com>
2015-07-20 01:21:35 +0400

    Api v0.2.11

--- private/api/methods/table.php
+++ private/api/methods/table.php
@@ -11,6 +11,7 @@ class table extends apiBaseClass{
 
     function setList($Params){
         $retJson = $this->createJson();
+        echo $Params->table_id;
         if(isset($Params->table_id)){
             $returnRequest = CO::SQL()->query('SELECT id_set, position FROM sets WHERE table_id =?',[['i',$Params->table_id]]);
             if (empty($returnRequest)) return null;
