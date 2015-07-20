129777ed8c911ab9fe4cf4fc4d669b374c2bf3cd
smile <smile.nikitoz@gmail.com>
2015-07-20 11:08:54 +0400

    Api v0.3.7.1

--- private/api/apiBaseClass.php
+++ private/api/apiBaseClass.php
@@ -11,6 +11,7 @@
                 $num = strtolower($num);
                 foreach ($arr as $key=>$value){
                     $key = strtolower($key);
+                    echo $value.'<br>';
                     $JsonObject->$num->$key = $value;
                 }
 
