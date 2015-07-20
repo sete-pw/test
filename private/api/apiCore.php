bfaa46e4d24e18864c0861b5e09876bca20c77fc
smile <smile.nikitoz@gmail.com>
2015-07-20 14:08:35 +0400

    Api v0.3.9.2

--- private/api/apiCore.php
+++ private/api/apiCore.php
@@ -73,6 +73,6 @@
                 $resultMethod->$status = ApiConstants::$ERROR_NOT_FOUND_METHOD;
                 $resultMethod->params = $this->apiFunctionParams;
             }
-            return json_encode($resultMethod,JSON_UNESCAPED_UNICODE);
+            return mb_detect_encoding(json_encode($resultMethod,JSON_UNESCAPED_UNICODE));
         }
     }
