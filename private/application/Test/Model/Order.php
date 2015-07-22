defcbca540ceae0db97f34aad1d608c16f42cd15
smile <smile.nikitoz@gmail.com>
2015-07-22 16:42:39 +0400

    [API] добавли user_id в odrer.getList

--- private/application/Test/Model/Order.php
+++ private/application/Test/Model/Order.php
@@ -11,7 +11,7 @@
 		function getList(){
 			if (\CO::AUTH()->admin()) {
 				$returnRequest = $this->QUERY("
-SELECT id_order_set, order_id, orders.price, table_id, set_id
+SELECT id_order_set, order_id, orders.price, table_id, set_id, user_id
 FROM orders
 INNER JOIN order_sets ON orders.id_order = order_sets.order_id
 INNER JOIN sets ON sets.id_set = order_sets.set_id
